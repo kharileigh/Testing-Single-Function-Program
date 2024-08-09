d120d26dff3b682f9c24269842ce5974a0e0cd14
kharileigh <kharileighmiller@gmail.com>
2024-08-09 16:12:26 +0100

    Design & Test Function -- completed solo

--- lib/diary.py
+++ lib/diary.py
@@ -1,19 +1,21 @@
 def diary(notes, tasks):
 
-    notes = []
-    tasks = ""
     
      # NO INPUT / 1 ARGUMENT
-    if notes == [] or tasks == "":
+    if notes == [] and tasks == "":
         raise Exception("Please enter text and word to search for")
+    elif notes == [] and tasks != "":
+        raise Exception("Please enter your notes")
+    elif notes != [] and tasks == "":
+        raise Exception("Please enter text to search for")
     
     # TWO ARGUMENTS
-    else:
+    # FOR LOOP -- to check every string in the list of notes
+    for note in notes:
+        print(note)
+        # YES - word in string, retun string
+        if tasks in note:
+            return note
         
-        # FOR LOOP -- to check every string in the list of notes
-        for note in notes:
-
-            # IF - to check if word is in current string
-            if tasks in note:
-                return note
-            return f"{tasks} is not in notes"
+    # NO - return message not found    
+    return f"{tasks} is not in notes"
--- tests/test_diary.py
+++ tests/test_diary.py
@@ -17,19 +17,18 @@ def test_one_argument_given_text():
     with pytest.raises(Exception) as err:
         diary([], "#TODO")
     error_message = str(err.value)
-    assert error_message == "Please enter text and word to search for"
+    assert error_message == "Please enter your notes"
 
 def test_one_argument_given_notes():
     with pytest.raises(Exception) as err:
         diary(["#TODO", "Python Foundations", "Golden Square"], "")
     error_message = str(err.value)
-    assert error_message == "Please enter text and word to search for"
+    assert error_message == "Please enter text to search for"
 
 
 #----------------- TWO ARGUMENTS
-def two_arguments_given():
-    notes_list = ["#TODO - 9 Aug", "Python Foundations", "Golden Square"]
-    tasks = "#TODO"
-    result = diary(notes_list, tasks)
+def test_two_arguments_given_word_not_found():
+    assert diary(["9 Aug", "Python Foundations", "Golden Square"], "#TODO") == "#TODO is not in notes"
 
-    assert result == "#TODO - 9 Aug"
+def test_two_arguments_given_word_found():
+    assert diary(["#TODO..List", "Python Foundations", "Golden Square"], "#TODO") == "#TODO..List"
